cabce4fb50a01c253092abbca31f2e2531a1a088
Rafael Melo <rafamsilva@live.com>
2017-04-19 15:10:31 -0300

    Novo commit boladao

--- ex/component.jsx
+++ ex/component.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 
 export default props => (
-    <h1>Primeiro Componente</h1>
+    <div>
+        <h1>Primeiro Componente</h1>
+        <h2>Sou um bosta</h2>
+    </div>
 )
 
 export const Segundo = props => <h1>Segundo Componente!</h1>
