92805a92d1b2699ae2240b2bc6e7f76fee3195eb
Rafael Melo <rafamsilva@live.com>
2017-04-19 16:54:31 -0300

    componentes controlados/nao controlados

--- ex/index.jsx
+++ ex/index.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import ClassComponent from './classComponent'
+import Field from './field'
 
 
 ReactDOM.render(
     <div>
-        <ClassComponent label='Contador' initialValue={10} />
+        <Field initialValue={"Teste"} />
     </div>
     ,document.getElementById('app')
 )
